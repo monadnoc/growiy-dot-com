6a3de099119ed7e576ddf5f0cbaac3c0efe347fc
monadnoc <jordannelson1.2014@u.northwestern.edu>
2019-04-05 17:11:46 -0500

    Fixed form thanks

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -7,7 +7,8 @@ export default () => (
       <div className="container">
         <div className="content">
           <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <p>If you have a specific question or need specific help, we will review your submission and contact you via email</p>
+          <p>If you are providing advice on the features you would like to see Growiy develop for hobbyist cannabis growers, thanks!</p>
         </div>
       </div>
     </section>
